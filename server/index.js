f6963b77469903d47fa6c754924c70d4c7ebf649
gbedad <gerald.berrebi@gmail.com>
2023-04-05 13:03:59 +0300

    commit server path

--- server/index.js
+++ server/index.js
@@ -16,5 +16,5 @@ app.listen(process.env.PORT || 3001, () => {
 app.use(express.static(path.join(__dirname, 'server/client/build')));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+  res.sendFile(path.resolve(__dirname, './server/client/build', 'index.html'))
 })
